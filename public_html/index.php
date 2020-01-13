543c7e82d8c5c82082c636d289d1c27bac852f6c
jtonello <john@johntonello.com>
2020-01-13 15:19:03 -0500

    First commit

---
+++ public_html/index.php
@@ -0,0 +1,16 @@
+<h1>Hello Cloudreach!</h1>
+<h4>Attempting MySQL connection from php...</h4>
+<?php
+$host = 'mysql';
+$user = 'otherUser';
+$pass = 'password';
+$db = "dbtest";
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL successfully!";
+}
+
+?>
